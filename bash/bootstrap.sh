d83c6bb06420462f85ae504f505e826aad216b5b
Tomas Susanka <tsusanka@gmail.com>
2014-04-29 08:21:49 +0200

    Bash: grub timeout set to 1s in bootstrap script

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -3,4 +3,8 @@
 # check if chef is installed, if not install it
 if ! type "chef-solo" &> /dev/null; then
 	curl -L --silent https://www.opscode.com/chef/install.sh | sudo bash
+	sudo bash -c 'echo "
+GRUB_HIDDEN_TIMEOUT_QUIET=true
+GRUB_TIMEOUT=1
+	" >> /etc/default/grub; sudo update-grub'
 fi
